7319eefa136b5a308cc3b65007cf6624fc6b5f75
wavyrn1 <54137267+wavyrn1@users.noreply.github.com>
2019-09-04 02:50:34 -0400

    added the current state of the project
    not in a usable state yet, documentation is sparse except in the Room class, however, shall be added soon

---
+++ shroudserver/Main.java
@@ -0,0 +1,11 @@
+package shroudserver;
+
+/**
+ * Shroud is a secure messaging service that records 0 user data and provides end-to-end encryption
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
